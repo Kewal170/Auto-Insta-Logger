7dc1e83cf1a087ee4e7d0888479d63defa65db5f
Kewal Suthar <102758565+Kewal170@users.noreply.github.com>
2022-08-08 21:35:26 +0530

    Update auto_insta_login.py

--- auto_insta_login.py
+++ auto_insta_login.py
@@ -12,13 +12,13 @@ driver.get("https://www.instagram.com/")
 def user_name():  # This function will type your username into username field
     username = driver.find_element("name", "username")
     username.send_keys(Keys.RETURN)
-    username.send_keys("<>Your username here<>")
+    username.send_keys("<>Your username here<>") # Add your username here
 
 
 def pass_word():  # This function will type your password into password field
     password = driver.find_element("name", "password")
     password.send_keys(Keys.RETURN)
-    password.send_keys("<>Your password here<>")
+    password.send_keys("<>Your password here<>") # Add your password here
     password.send_keys(Keys.RETURN)
 
 
